6f8b6e1294242b850222c75670f4ba1b1999a5f7
SaigyoujiYuyuko <hgk-saigyoujiyuyuko@outlook.com>
2024-02-04 12:44:52 +0800

    fmt file

--- example_config/aliyun-vm/main.tf
+++ example_config/aliyun-vm/main.tf
@@ -1,22 +1,22 @@
 terraform {
   required_providers {
     aliyun = {
-      source = "aliyun/alicloud"
+      source  = "aliyun/alicloud"
       version = "~> 1.217.0"
     }
   }
 }
 
 provider "aliyun" {
-    access_key = "accId"
-    secret_key = "accKey"
-    region = "cn-shenzhen"
+  access_key = "accId"
+  secret_key = "accKey"
+  region     = "cn-shenzhen"
 }
 
 resource "alicloud_instance" "example_instance" {
-  instance_name              = "example"
-  host_name                  = "ciallo-vm"
-  password                   = "c1all0~(<.w<)n!"
+  instance_name = "example"
+  host_name     = "ciallo-vm"
+  password      = "c1all0~(<.w<)n!"
 
   image_id                      = "fedora_39_x64_20G_alibase_20231121.vhd"
   instance_type                 = "ecs.t5-lc2m1.nano"
@@ -28,9 +28,9 @@ resource "alicloud_instance" "example_instance" {
   security_enhancement_strategy = "Deactive"
   #data_disks {}
 
-  instance_charge_type       = "PostPaid"
-  internet_charge_type       = "PayByTraffic"
-  spot_strategy              = "SpotWithPriceLimit"
-  spot_price_limit           = "0.02"
-  spot_duration              = "1"
+  instance_charge_type = "PostPaid"
+  internet_charge_type = "PayByTraffic"
+  spot_strategy        = "SpotWithPriceLimit"
+  spot_price_limit     = "0.02"
+  spot_duration        = "1"
 }
